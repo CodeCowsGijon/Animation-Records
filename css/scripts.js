db80526d75e6f60899e9a5ad210ccfdd988af762
MktCoto <helly.frank.tang@gmail.com>
2021-10-28 18:15:14 +0200

    add img actualizadasy letras centradas

--- css/scripts.js
+++
@@ -1,8 +0,0 @@
-
-
-let miBoton = document.getElementById("miInput")
-
-miInput.addEventListener("input";saludar);
-miInput.addEventListener("input";despedir);
-
-function saludar()
---
+++ javascripts/scripts.js
@@ -0,0 +1,13 @@
+
+
+let miInput = document.getElementById("miInput")
+
+miInput.addEventListener("input", function(event){
+    console.log(event);
+
+miInput.addEventListener("input")
+});
+
+function saludar(){
+    alert("Hola, como estas?");
+}
