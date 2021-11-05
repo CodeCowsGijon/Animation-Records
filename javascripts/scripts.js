67839cd1091125ada9c88658f965ac590354e6fa
MktCoto <helly.frank.tang@gmail.com>
2021-11-05 03:11:46 +0100

    add javaScripts

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,9 +1,26 @@
-
+var friendList =[];
+/* guardar_localstorage(id1); */
 
 function mostrarId1(){
     let id1= document.getElementById("id1").value;
     console.log(id1)
-
+    friendList.push(id1)
+    localStorageFriendList(friendList);
 
 }
-    
+
+function getfriendList(){
+    var storedList = localStorage.setItem('localFriendList');
+    if(storedList == null){
+        friendList = [];
+    }else{
+        friendList = (storedList);
+    }
+    return friendList;
+
+}
+
+function localStorageFriendList(plist){
+    localStorage.setItem('localFriendList', plist);
+}
+
